979d4256d4e802f2b42dbe343406f4594ac7bd3c
MAGomes95 <alexandre95mario@gmail.com>
2023-11-14 21:00:41 +0000

    fix preprocessing pipeling bug with LabelEncoder class

--- src/data/preprocessing.py
+++ src/data/preprocessing.py
@@ -15,9 +15,36 @@ from sklearn.feature_selection import (
     mutual_info_classif
 )
 from sklearn.pipeline import Pipeline
-from logger import get_console_logger
+from sklearn.base import (
+    TransformerMixin,
+    BaseEstimator
+)
+#from logger import get_console_logger
 
-logger = get_console_logger()
+#logger = get_console_logger()
+
+
+class LabelEncoderTransformer(BaseEstimator, TransformerMixin):
+    """
+    This class implements a classic LabelEncoder class,
+    with the turn-around of taking X,y as arguments instead
+    of only y.
+    """
+    def __init__(self):
+        self.encoder = LabelEncoder()
+
+    def fit(self, X, y):
+        self.encoder = self.encoder.fit(y)
+        return self
+    
+    def transform(self, X, y):
+        return self.encoder.transform(y)
+
+    def fit_transform(self, X, y):
+        self = self.fit(X, y)
+        output = self.transform(X, y)
+        return X, output.reshape(-1, 1)
+
 
 def get_features_and_target(
         path_to_input: Optional[Path],
@@ -28,7 +55,7 @@ def get_features_and_target(
     """
     dataset = pd.read_csv(path_to_input)
     features = dataset[np.setdiff1d(dataset.columns, [target])]
-    target = dataset[target]
+    target = dataset[target].values
 
     return features, target
 
@@ -46,7 +73,7 @@ def get_preprocessing_pipeline() -> Pipeline:
         ("interaction features", PolynomialFeatures(interaction_only=True, include_bias=False)),
         ("feature selection", SelectKBest(mutual_info_classif, k=5)),
         ("scaling", MinMaxScaler()),
-        ("label encoding", LabelEncoder())
+        ("label encoding", LabelEncoderTransformer()),
     ]
 
     return Pipeline(steps=pipeline_steps)
@@ -58,7 +85,6 @@ if __name__ == "__main__":
     
     preprocessing_pipeline = get_preprocessing_pipeline()
 
-    preprocessing_pipeline.fit(features, target)
-    X, y = preprocessing_pipeline.transform(features, target)
+    X, y = preprocessing_pipeline.fit_transform(features, target)
 
-    print(X.head())
+    print(X[:5], y[:5], sep="\n")
